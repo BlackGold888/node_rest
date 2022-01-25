37f046713a8834433204805b7c3eacc4af87099d
blackgold888 <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>
2022-01-25 21:35:25 +0200

    Add dotenv config

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
+const express = require('express');
 const db = require('./models');
 const session = require('express-session');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
